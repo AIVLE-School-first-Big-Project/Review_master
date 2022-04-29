81392accc2b8e70973b9a10501af555d0b2383ac
SANGHEE <tkdgmlgg7@gmail.com>
2022-04-29 16:12:50 +0900

    update add-ons

--- WEB2/board/views.py
+++ WEB2/board/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from accountapp.models import Member
 from board.models import QABoard
-
+from django.db.models import Max 
 
 def board_list(request):
     postlist = QABoard.objects.all()
@@ -13,12 +13,29 @@ def board_list(request):
 
 def board_write(request):
     if request.method=="POST":
-        post_num = len(QABoard.objects.all())
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        new_list = QABoard(id=post_num+1, user_id=request.session['user_id'], title=title, content=content)
-        new_list.save()
-        return redirect('/app/board/')
+        # 관리자가 답글을 다는 경우 
+        if request.session['user_class']==99:
+            post_id = request.POST.get('question_id')
+            new_answer = QABoard.objects.get(id=post_id)
+            
+            answer_title = request.POST.get('answer_title')
+            answer_content = request.POST.get('answer_content')
+            
+            new_answer.answer_title = answer_title
+            new_answer.answer_content = answer_content
+            new_answer.save()
+            return redirect('/app/board/')
+
+        # 사용자가 질문글을 작성하는 경우
+        else:
+            max_id_post = QABoard.objects.aggregate(id=Max('id'))
+            post_num = max_id_post['id']
+            # post_num = len(QABoard.objects.all())
+            title = request.POST.get('title')
+            content = request.POST.get('content')
+            new_list = QABoard(id=post_num+1, user_id=request.session['user_id'], title=title, content=content)
+            new_list.save()
+            return redirect('/app/board/')
     return render(request, "board/board_write.html")
 
 def board_detail(request, id):
