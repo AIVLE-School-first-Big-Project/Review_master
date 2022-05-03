927f8c82fb07919b0f537ae9f4b66af923b16a4d
dbswl2324 <leej23243@gmail.com>
2022-05-03 11:09:49 +0900

    UPDATE 더보기

---
+++ WEB2/static/assets/js/result.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.review_tr').each(function(){
+        var content = $(this).children('.content');
+        var content_txt = content.text();
+        var content_txt_short = content_txt.substring(0,150)+"...";
+        var btn_more = $('</tr><tr><td><a href="javascript:void(0)" class="more" style="font-size:0.5rem">더보기</a></td></tr></rs>');
+
+        
+        $('.review_bth').append(btn_more);
+        
+        if(content_txt.length >= 150){
+            content.html(content_txt_short)
+            
+        }else{
+            btn_more.hide()
+        }
+        
+        btn_more.click(toggle_content);
+        // 아래 bind가 안 되는 이유는??
+        // btn_more.bind('click',toggle_content);
+
+        function toggle_content(){
+            if($(this).hasClass('short')){
+                // 접기 상태
+                $(this).html('더보기');
+                content.html(content_txt_short)
+                $(this).removeClass('short');
+            }else{
+                // 더보기 상태
+                $(this).html('접기');
+                content.html(content_txt);
+                $(this).addClass('short');
+
+            }
+        }
+    });
+});
